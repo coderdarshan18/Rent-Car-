957a9f1b303eeb5ef4de1c1e283084a2d94ca4e7
Darshan Belgaonkar <belgaonkardarshan20@gmail.com>
2021-09-23 19:50:16 +0530

    Secound commit

--- force-app/main/default/lwc/carSearchResult/carSearchResult.js
+++ force-app/main/default/lwc/carSearchResult/carSearchResult.js
@@ -1,11 +1,10 @@
 import { LightningElement,api, track, wire } from 'lwc';
 import getCars from '@salesforce/apex/CarSearchResultController.getCars';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-
-
 export default class CarSearchResult extends LightningElement {
     @api carTypeId;
     @track cars;
+    @track selectedCarId;
 
     @wire(getCars,{carTypeId :'$carTypeId'})
     wiredCars({data,error}){
@@ -14,6 +13,10 @@ export default class CarSearchResult extends LightningElement {
         }else if(error){
             this.showToast('ERROR',error.body.message,'error');
         }
+    }
+    carSelectHandler(event){
+        const cardId = event.detail;
+        this.selectedCarId = cardId;
     }
 
     showToast(title,message,variant){

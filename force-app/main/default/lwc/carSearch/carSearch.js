6b04530b8707b31a7ed23cfe870e04701adf991e
Darshan Belgaonkar <belgaonkardarshan20@gmail.com>
2021-09-23 17:53:05 +0530

    first commit

--- force-app/main/default/lwc/carSearch/carSearch.js
+++ force-app/main/default/lwc/carSearch/carSearch.js
@@ -1,3 +1,8 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class CarSearch extends LightningElement {}
+export default class CarSearch extends LightningElement {
+    @track carTypeId='';
+    carTypeSelectHandler(event){
+        this.carTypeId =event.detail;
+    }
+}
---
+++ force-app/main/default/lwc/carSearchResult/carSearchResult.js
@@ -0,0 +1,33 @@
+import { LightningElement,api, track, wire } from 'lwc';
+import getCars from '@salesforce/apex/CarSearchResultController.getCars';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+
+export default class CarSearchResult extends LightningElement {
+    @api carTypeId;
+    @track cars;
+
+    @wire(getCars,{carTypeId :'$carTypeId'})
+    wiredCars({data,error}){
+        if(data){
+            this.cars= data;
+        }else if(error){
+            this.showToast('ERROR',error.body.message,'error');
+        }
+    }
+
+    showToast(title,message,variant){
+        const evt = new ShowToastEvent({
+            title : title,
+            message : message,
+            variant : variant,
+        });
+    }
+    get carsFound(){
+        if(this.cars){
+            return true;
+
+        }
+        return false;
+    }
+}
